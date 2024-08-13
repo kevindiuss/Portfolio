6a0e12d204b35e2c65c3b006045a275e15c79734
kevindiuss <kevindiuss2@gmail.com>
2024-08-13 11:45:12 -0500

    inserto particulas

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {
+    const scroll = window.scrollY;
+
+    const barraNav = document.querySelector('.barra');
+
+    if(scroll > 300) {
+        barraNav.classList.add('fixed-top');
+    } else {
+        barraNav.classList.remove('fixed-top');
+    }
+}
+
+const hamburguer = document.querySelector('.hamburguer-menu svg');
+hamburguer.addEventListener('click', function() {
+    const menuPrincipal = document.querySelector('.navegacion');
+    menuPrincipal.classList.toggle('mostrar');
+})
+
